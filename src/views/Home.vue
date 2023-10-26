1b8212de20c71b7b23c165d283e779f3c135b573
Eduardo_cas123 <juegoseduardo09@gmail.com>
2023-10-25 23:56:24 -0400

    changes Menu and Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,8 @@
 <template>
-  <Menu/>
   <List/>
 </template>
 
 <script setup>
   import HelloWorld from '@/components/HelloWorld.vue'
-  import Menu from '@/components/Menu.vue'
   import List from './List.vue'
 </script>
